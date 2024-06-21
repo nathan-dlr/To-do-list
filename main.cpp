16605b22419560a9166b14bb5ef56d68a0ad15e5
nathan-dlr <nathan.delarosa89@gmail.com>
2024-06-20 20:36:14 -0500

    created check boxes for tasks and ability to type in task.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 // Start of wxWidgets "Hello World" Program
+#include <iostream>
 #include  <wx/wx.h>
 #include <wx/artprov.h>
+
+int ADD_TASK_Y = 0;
+int TASK_Y = -50;
 
 class MyApp : public wxApp
 {
@@ -17,18 +21,17 @@ public:
     MyFrame();
 
 private:
-    void OnHello(wxCommandEvent& event);
+    wxButton* addTask;
+    wxBoxSizer* sizer;
+    wxCheckBox* task;
+    wxTextCtrl* typeTask;
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
     void OnAddTask(wxCommandEvent& event);
+    void OnEnter(wxCommandEvent& event);
+    void CreateTask();
 };
 
-//"don't need to define identifiers for "About" and "Exit", as wxWidgets already predefines standard values such as wxID_ABOUT and wxID_EXIT "
-enum
-{
-    ID_Hello = 1,
-    ID_AddTask = 2l
-};
 
 //"this function is called  upon application startup and creates the main window"
 bool MyApp::OnInit()
@@ -36,7 +39,7 @@ bool MyApp::OnInit()
     //"there is no memory leak in this: wxWidgets takes ownership of all the window objects and destroys them automatically when the corresponding on-screen window is destroyed."
     MyFrame *frame = new MyFrame();
     frame->SetClientSize(1000, 600);
-    //"frames are hidden by default to allow filling them to be filled with contents before showing everything at once."
+     //"frames are hidden by default to allow filling them to be filled with contents before showing everything at once."
     //"Show() must be called for the application to appear"
     frame->Show(true);
     //"returning true allows the application to start running"
@@ -46,9 +49,6 @@ bool MyApp::OnInit()
  MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "To-Do") {
     //"menu pointers don't need to (and, in fact, must not) be destroyed because they are owned by the menu bar, which is itself owned by the frame, which is owned, i.e. will be destroyed, by wxWidgets."
     wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
     menuFile->Append(wxID_EXIT);
 
     wxMenu *menuHelp = new wxMenu;
@@ -58,23 +58,34 @@ bool MyApp::OnInit()
     menuBar->Append(menuFile, "&File");
     menuBar->Append(menuHelp, "&Help");
 
-    wxButton *addTask = new wxButton(this, wxID_ADD, "Add Task", wxPoint(0,0), wxSize(500,50));
+    addTask = new wxButton(this, wxID_ADD, "Add Task", wxPoint(0,0), wxSize(500,50));
     addTask->SetBitmap(wxArtProvider::GetBitmap(wxART_PLUS, wxART_BUTTON));
 
-    wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+    sizer = new wxBoxSizer(wxHORIZONTAL);
     sizer->Add(addTask, 1);
 
      SetSizerAndFit(sizer);
 
 
-
-
-    //"We have to connect our event handlers to the events we want to handle in them. We do this by calling Bind() to send all the menu events (identified by wxEVT_MENU event type) with the specified ID to the given function."
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+     //"We have to connect our event handlers to the events we want to handle in them. We do this by calling Bind() to send all the menu events (identified by wxEVT_MENU event type) with the specified ID to the given function."
     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
 
     addTask->Bind(wxEVT_BUTTON, &MyFrame::OnAddTask, this, wxID_ADD);
+}
+
+
+void MyFrame::CreateTask() {
+    typeTask = new wxTextCtrl(this, wxID_ANY,"Enter Task", wxPoint(0, TASK_Y), wxSize(500, 50), wxTE_PROCESS_ENTER);
+    typeTask->Bind(wxEVT_TEXT_ENTER, &MyFrame::OnEnter, this);
+
+}
+
+void MyFrame::OnEnter(wxCommandEvent &event) {
+    //crashes when there are two task descriptions open at once
+    wxString taskDescription = typeTask->GetValue();
+    typeTask->Destroy();
+    task = new wxCheckBox(this, wxID_ANY, taskDescription, wxPoint(50, TASK_Y), wxSize(500, 50));
 }
 
 void MyFrame::OnExit(wxCommandEvent& event)
@@ -88,10 +99,10 @@ void MyFrame::OnAbout(wxCommandEvent& event)
                  "About Hello World", wxOK | wxICON_INFORMATION);
 }
 
-void MyFrame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello world from wxWidgets!");
-}
-
 void MyFrame::OnAddTask(wxCommandEvent& event) {
+    //add an if-else statement thatll stop the button from moving when its at the bottom of the screen
+    ADD_TASK_Y += 50;
+    TASK_Y += 50;
+    CreateTask();
+    addTask->wxWindow::Move(0,ADD_TASK_Y, wxSIZE_USE_EXISTING);
 }
